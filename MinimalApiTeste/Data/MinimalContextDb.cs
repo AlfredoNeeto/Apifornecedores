b9466289f02c201ac84b21acc9da4af5fb16141e
Alfredo Neto <104031356+alfredo-newteam@users.noreply.github.com>
2022-12-16 14:48:16 -0400

    Apifornecedores

---
+++ MinimalApiTeste/Data/MinimalContextDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiTeste.Models;
+
+namespace MinimalApiTeste.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) : base(options) { }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fornecedor>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("Varchar(200)");
+
+            modelBuilder.Entity<Fornecedor>()
+               .Property(p => p.Documento)
+               .IsRequired()
+               .HasColumnType("Varchar(14)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .ToTable("Fornecedores");
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
